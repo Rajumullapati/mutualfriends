0a94e0f808bbd16265f92ab9b4e4af3313a30445
Rajumullapati <34390830+Rajumullapati@users.noreply.github.com>
2022-03-02 11:03:30 -0600

    Updates

--- src/main/java/com/hadoop/mutualfriends/MutualFriendInvertedIndexMapReduceApp.java
+++ src/main/java/com/hadoop/mutualfriends/MutualFriendInvertedIndexMapReduceApp.java
@@ -11,7 +11,6 @@ import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
